ac2835f2c09fb16241d130d3d2c666bec1f5bef4
inwjob17 <inwjob17@gmail.com>
2026-03-17 16:09:56 +0700

    Finish

---
+++ Assets/script/CameraFollow.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CameraFllow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 3, -8);
+    public float smoothSpeed = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 desirePosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desirePosition, smoothSpeed * Time.deltaTime);
+
+        Quaternion targetRotation = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, smoothSpeed * Time.deltaTime);
+    }
+}
